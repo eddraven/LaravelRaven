9792f5c003a84bc22478552d589d47caebd92f0a
eddraven <eduardohr91@hotmail.com>
2019-08-31 22:49:30 -0500

    rutas 1

--- PWEB1/routes/web.php
+++ PWEB1/routes/web.php
@@ -15,4 +15,18 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('numero/{num?}',function($num='No ingresó ningún parámetro...'){
+	return 'Número ingresado por URL es : '.$num;
+})->where('num', '[0-9]+');
 
+
+//mi propio ejemplo
+	//ruta básica con vista
+	Route::get('fotos',function(){
+		return view('fotos');
+	});
+
+	//ruta con parametro a una vista (fotos)
+	Route::get('fotos/{nume}',function($numm='No ingresó ningún parámetro...'){
+		return view('fotos', ['num'=>$numm]);
+	});
